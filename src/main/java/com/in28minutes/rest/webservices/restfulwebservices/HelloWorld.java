a9e06a5dc04a3cab8526afcaab37eb77c089e450
schatzen <56854068+schatzen@users.noreply.github.com>
2021-03-08 19:04:22 +0900

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.*;
+
+//controller
+@RestController
+public class HelloWorld {
+
+//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+//    public String hellworld(){
+//        return "HelloWorld";
+//    }
+
+    @GetMapping(path = "/hello-world")
+    public String hellworld() {
+        return "HelloWorld";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean hellworldBean() {
+        return new HelloWorldBean("hello, world");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean hellworldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello, %s", name));
+    }
+
+
+}
