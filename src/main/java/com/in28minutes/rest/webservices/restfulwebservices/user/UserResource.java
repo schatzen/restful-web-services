7c5dd45b403680b6a2c356801fab5609fd8e778c
schatzen <56854068+schatzen@users.noreply.github.com>
2021-03-15 16:32:16 +0900

    .

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,49 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    // retrieveAllUsers
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+
+    }
+
+    // retrieveUser(int id)
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+        return user;
+    }
+
+    // Created
+    // input - details of user
+    // output = created & return the created URI
+    @PostMapping("/users")
+    public ResponseEntity<Object> createdUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        // 반환 값
+        return ResponseEntity.created(location).build();
+    }
+}
