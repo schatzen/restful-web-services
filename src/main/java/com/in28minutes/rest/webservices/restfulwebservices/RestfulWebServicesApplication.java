cf0789eaf696e51fed85d5e44f5a6ba7cdfc34a3
schatzen <gomieti@naver.com>
2021-05-06 14:33:10 +0900

    # Internationalization 적용

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,12 +2,44 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+    //  Internationalization - 01
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//        localeResolver.setDefaultLocale(Locale.US);
+//        return localeResolver;
+//    }
+
+    //  Internationalization - 02
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    //  Internationalization - 02 에선 필요하지 않아 주석처리.
+//    @Bean
+//    public ResourceBundleMessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("messages");
+//        return messageSource;
+//    }
+
 
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
@@ -1,10 +1,18 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //controller
 @RestController
 public class HelloWorld {
+
+    @Autowired
+    private MessageSource messageSource;
 
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 //    public String hellworld(){
@@ -24,6 +32,18 @@ public class HelloWorld {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean hellworldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello, %s", name));
+    }
+
+    //  Internationalization - 01
+//    @GetMapping(path = "/hello-world-internationalized")
+//    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+//        return messageSource.getMessage("good.morning.message", null, locale);
+//    }
+
+    //  Internationalization - 02
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 
 
